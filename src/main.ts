0bb97e736cf9e4e339272d9e5d9b1bc934f4fc30
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-22 21:38:53 -0500

    Add: example SRP - Principio de responsabilidad única

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './clean-code/04-homework.ts'
+import './SOLID/01-SRP.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
