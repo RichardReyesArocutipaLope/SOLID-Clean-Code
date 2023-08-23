daf8ff7482ebcd60b5011ef19c66f0f2e54b75fb
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-23 00:33:46 -0500

    Add: example Interface Segregation

---
+++ src/SOLID/04-segregation.ts
@@ -0,0 +1,36 @@
+
+interface Bird {
+    eat(): void;
+}
+
+interface FlyingBird {
+    fly(): void;
+}
+
+interface RunningBird {
+    run(): void;
+}
+
+interface swimmerBird {
+    swim(): void;
+}
+
+class Tucan implements Bird, FlyingBird {
+    public eat() { }
+    public fly() { }
+}
+
+class Humminbird implements Bird, FlyingBird {
+    public eat() { }
+    public fly() { }
+}
+
+class Ostrich implements Bird, RunningBird {
+    public eat() { }
+    public run() { }
+}
+
+class Penguin implements Bird, swimmerBird {
+    public eat() { }
+    public swim() { }
+}
